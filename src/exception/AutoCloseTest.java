1aa3cc04e00432ac095c5a38758c0488dccf7d5b
Soda <k.sua923@gmail.com>
2026-01-07 20:12:32 +0900

    예외처리 미루기, 사용자 정의 예외

--- src/exception/AutoCloseTest.java
+++ src/exception/AutoCloseTest.java
@@ -1,4 +1,12 @@
 package exception;
 
 public class AutoCloseTest {
+    public static void main(String[] args) {
+        AutoCloseObj obj = new AutoCloseObj();
+        try(obj){
+            throw new Exception(); // 강제 예외 발생
+        } catch (Exception e){
+            System.out.println("예외 부분");
+        }
+    }
 }
---
+++ src/exception/ThrowsException.java
@@ -0,0 +1,26 @@
+package exception;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ThrowsException {
+    public Class loadClass(String fileName, String className) throws // 두 예외를 메서드가 호출될 때 처리하도록 미룸
+            FileNotFoundException, ClassNotFoundException {
+        FileInputStream fis = new FileInputStream(fileName);
+        Class c = Class.forName(className);
+        return c;
+    }
+
+    public static void main(String[] args) {
+        ThrowsException test = new ThrowsException();
+        try {
+            test.loadClass("a.text", "java.lang.String");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
